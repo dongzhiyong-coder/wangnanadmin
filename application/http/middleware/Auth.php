ce153c7de63b68c332a9e61601dd31c4923086ec
dongzhiyong <dongzy@xinruiying.com>
2020-06-12 11:14:20 +0800

    实现了角色的无限极菜单功能

--- application/http/middleware/Auth.php
+++ application/http/middleware/Auth.php
@@ -12,22 +12,72 @@ class Auth
         //把用户菜单保存在session里面
         $db_config = require  "../config/db_config.php";
         $mysql = new \mysql($db_config);
-        $menus = $mysql->table('pc_menu')->select();
-        //按层级关系组织菜单数组
-        $page_menu = [];
-        foreach ($menus as $menu){
-            if($menu['parent_menu_id']==0){
-                $page_menu[] = $menu;
-            }
+        //找到登录用户所有的角色
+        $roles = $mysql->table('pc_user_role')->where("user_id=".Session::get('uid'))->select();
+        $role_list = [];
+        foreach ($roles as $role){
+            $role_list[] = $role['role_id'];
         }
-        foreach ($page_menu as $key=>$page_one){
-            foreach ($menus as $menu){
-                if($menu['parent_menu_id']==$page_one['menu_id']){
-                    $page_menu[$key]['child'][] = $menu;
-                }
-            }
-        }
+        //通过角色找到对应的菜单集合 用In
+        $sql = "SELECT menu_id from pc_role_menu where role_id in(".implode(',',$role_list).")";
+        $role_menus = $mysql->query($sql);
+        //获取页面菜单
+        $page_menu = $this->getPageMenu($role_menus);
+        //echo "<pre>";print_r($page_menu);die;
         Session::set('menu_list',$page_menu);
         return $next($request);
+    }
+
+
+
+    /**
+     * 返回页面菜单 按层级关系显示的
+     * 角色的菜单id集合
+     * @param $role_menus
+     */
+    private function getPageMenu($role_menus){
+        $role_menu_list = $page_menu = [];
+        foreach ($role_menus as $role_menu){
+            $menu = $this->getMenu($role_menu['menu_id']);
+            $role_menu_list[] = $menu;
+        }
+        $page_menu = $this->getlistMenu($role_menu_list);
+        return $page_menu;
+    }
+
+    /**
+     * 返回页面菜单
+     * @param $menus
+     * @param $pid
+     */
+    public function getlistMenu($menus,$pid=0)
+    {
+        $array = [];
+        //重置key 这样前端好遍历
+        $mk = 0;
+        foreach ($menus as $key => $item) {
+            if( $item['parent_menu_id'] == $pid )
+            {
+                $array[$mk] = $item;
+                //提升性能 处理过的就剔除 不用让他下次去循环
+                unset($menus[$key]);
+                $array[$mk]['son'] = $this->getlistMenu( $menus, $item['menu_id']);
+                $mk++;
+            }
+        }
+        return $array;
+    }
+
+
+
+    //根据id获取到菜单实体数据
+    private function getMenu($menu_id){
+        $db_config = require  "../config/db_config.php";
+        $mysql = new \mysql($db_config);
+        $menu = $mysql->table('pc_menu')->where("menu_id=".$menu_id)->find();
+        if(!empty($menu)){
+            return $menu;
+        }
+        return null;
     }
 }
